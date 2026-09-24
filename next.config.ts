55c064fc123b9e770efadbb5b3d5a6beeeee551c
Mariah0-0 <mariahkhalifa05@gmail.com>
2026-09-24 22:14:37 +0530

    add redirects from old urls

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/ac-repair",
+        destination: "/services/ac-repair",
+        permanent: true,
+      },
+      {
+        source: "/washing-machine-repair",
+        destination: "/services/washing-machine-repair",
+        permanent: true,
+      },
+      {
+        source: "/tv-repair",
+        destination: "/services/tv-repair",
+        permanent: true,
+      },
+      {
+        source: "/microwave-repair",
+        destination: "/services/microwave-repair",
+        permanent: true,
+      },
+      {
+        source: "/refrigerator-repair",
+        destination: "/services/refrigerator-repair",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
